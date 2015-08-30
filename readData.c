23e38ea554cf1d66ffac75ca81b1ec9dd127e0a7
crazyrabbitpei <willow111333@gmail.com>
2015-08-30 23:39:12 +0800

    can choose read origin data or delete-tag data

--- readData.c
+++ readData.c
@@ -2,14 +2,14 @@
 //1.detect html tag, convert it to gais record(maintain "id", "view count", "link cout/reference count" and "like count" ...)
 //2.function
 /*
-    -main
-    -for test
-    -read data
-    -delete html tag
-    -get detail(upload date,author,size,link...)
-    -convert to gais reocrd
+   -main
+   -for test
+   -read data
+   -delete html tag
+   -get detail(upload date,author,size,link...)
+   -convert to gais reocrd
 
-*/
+ */
 #include "readData.h"
 #include<stdio.h>
 #include<stdlib.h>
@@ -17,35 +17,40 @@
 #include<errno.h>
 #include<string.h>
 
-int ReadData(int command, char *ifile,char *ofile,int rep,char *reg,char *result){
+int ReadData(int command, char *ifile,int rep,char *reg,char *result){
 
-    FILE *stream;
-    char data[SIZE];
-    int totalsize=0,size=0,count=0;
-    stream = fopen(ifile,"r");
-    if(stream==NULL){
-        error(errno);
-        return -1;   
-    }
-    /*
-    output = fopen(ofile,"w");
-    if(output==NULL){
-        error(errno);
-        return -1;
-    }
-    */
-    size = fread(data,sizeof(char),SIZE,stream);
+	FILE *stream;
+	char *data;
+	int totalsize=0,size=0,count=0;
+	stream = fopen(ifile,"r");
+	if(stream==NULL){
+		error(errno);
+		return -1;   
+	}
+	if(reg==NULL){
+		size = fread(result,sizeof(char),SIZE,stream);
+		if(rep==YES){
+			DeleteNewline(result,size);
+		}
+		return size;
+	}
+	else{
+		data = malloc(sizeof(char)*SIZE);
+		size = fread(data,sizeof(char),SIZE,stream);
+	}
+	if(rep==YES){
+		DeleteNewline(data,size);
+	}
 
-    if(rep==YES){
-        DeleteNewline(data,size);
-    }
-    DeleteTag(data,ofile,reg,result);
-    fclose(stream);
-    return 0;
+	DeleteTag(data,reg,result);
+
+	free(data);
+	fclose(stream);
+	return size;
 }
 void print(char *data){
-    printf("read data:%s\n",data);
+	printf("read data:%s\n",data);
 }
 void error(int err){
-    printf("error message:%s\n",strerror(err));
+	printf("deleteTag error message:%s\n",strerror(err));
 }
