5ba774aef29f6afe44c8f44abf528cc8e344b49d
crazyrabbitpei <willow111333@gmail.com>
2015-09-08 20:33:03 +0800

    add size.h file

---
+++ size.h
@@ -0,0 +1,14 @@
+//read data
+#define IMPORT_DATA_LEN 7340032//7M
+//toType
+#define AUTHOR_LEN 20
+#define LAN_LEN 20
+#define COLUMN_NUM 3
+#define COLUMN_LEN 20
+//Mongo connect
+#define DB_LEN 20
+#define COLL_LEN 20
+#define TODB_RESULT_LEN 7340032
+#define TODB_IMPORT_LEN 7340032
+#define TODB_READ_PER 1048576
+#define MONGO_SERVER_P 28017
